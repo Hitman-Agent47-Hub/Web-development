ed74b64e756bbc7cea62b45d7596841cfc243248
Hitman-Agent47-Hub <144259395+Hitman-Agent47-Hub@users.noreply.github.com>
2023-11-07 12:01:06 +0500

    class work commit

---
+++ Class Work 07-11-2023/my_first.js
@@ -0,0 +1,47 @@
+console.log("Zaid Ahmed");
+console.log( typeof("Zaid Ahmed"));
+console.log( typeof(1));
+var a=5;
+console.log(typeof(a));
+console.log(typeof(true));
+var z;
+console.log(typeof(z));
+
+var currentTime = 10;
+console.log(typeof(currentTime>9 && currentTime<17));
+console.log(currentTime>9 && currentTime<17);
+console.log(currentTime>10 && currentTime<17);
+
+console.log(currentTime>9 || currentTime<17);
+console.log(currentTime>10 || currentTime<9);
+
+console.log(22%5);//2
+console.log(22/5);//2
+console.log(typeof(22/5));//2
+
+petHungry=true;
+petHungry=!petHungry;
+console.log(petHungry);
+console.log(5==5);
+console.log(5=="5");
+console.log(5===5);
+console.log(5==="5");
+console.log("inter"+"net");
+console.log("iter"+"ation");
+console.log("365"+"days");
+console.log(365+"days");
+console.log("1"+"2");
+console.log(1+2);
+var overtime=1;
+overtime+=2;
+overtime+=1;
+overtime+=2;
+overtime+=3;
+console.log(overtime);
+var longString="";
+longString+="once";
+longString+="upon";
+longString+="a";
+longString+="time";
+longString+="...";
+console.log(longString);//once upon a time

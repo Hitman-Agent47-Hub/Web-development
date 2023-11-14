ee353d8516ff77a061c9fdc4c58eea92ca408b6c
Hitman-Agent47-Hub <144259395+Hitman-Agent47-Hub@users.noreply.github.com>
2023-11-14 13:29:21 +0500

    Class Work 4th commit

---
+++ Class Work 14-11-23/Objects.js
@@ -0,0 +1,42 @@
+var table={
+    legs:3,
+    color:"brown",
+    priceUSD:100,
+}
+table.color1="blue";
+console.log(table);
+console.log(table.color);
+console.log(table["color1"]);
+//---------------------------------------------------
+var car={};
+car.color="red";
+car["color"]="green";
+car ["speed"]=200; //overwrite because of same name
+car.speed=100;
+console.log(car);
+//--------------------------------------------------------------
+var arrOfKeys=['speed','altitude','color'];
+var drone={
+    speed:100,
+    altitude:200,
+    color:"red",
+}
+for(var i=0;i<arrOfKeys.length;i++){
+    console.log(drone[arrOfKeys[i]]);
+}
+//------------------------------------------------------------
+var fruits=[];
+fruits.push("apple");
+fruits.push("pear");
+fruits.pop();
+console.log(fruits);
+//--------------------------------------------------------
+function arrayBuilder(one,two,three){
+var arr=[];
+arr.push(one);
+arr.push(two);
+arr.push(three);
+return arr;
+}
+var simpleArr= arrayBuilder('apple','pear','plum');
+console.log(simpleArr);

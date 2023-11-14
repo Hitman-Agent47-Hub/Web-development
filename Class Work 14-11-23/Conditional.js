9f46134fd237856f216cc62e440ae8524d9740fb
Hitman-Agent47-Hub <144259395+Hitman-Agent47-Hub@users.noreply.github.com>
2023-11-14 08:47:20 +0500

    Class Work 1st commit

---
+++ Class Work 14-11-23/Conditional.js
@@ -0,0 +1,15 @@
+var result=40;
+if(result>40){
+    console.log("Congratulations you passed.");
+}
+    else{
+        console.log("Unfortunately you did not pass.");
+    }
+
+    var food="hot";
+    if(food=="hot"){
+        console.log("Too hot!");
+    }
+    else if(food=="cold"){
+        
+    }	

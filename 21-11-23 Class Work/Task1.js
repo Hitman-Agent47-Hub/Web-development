893fde2be76276e5c8492934e020cc313e06849f
Hitman-Agent47-Hub <144259395+Hitman-Agent47-Hub@users.noreply.github.com>
2023-11-21 09:52:40 +0500

    Class Work 21-11-23 2nd commit

---
+++ 21-11-23 Class Work/Task1.js
@@ -0,0 +1,40 @@
+var clothes=[];
+clothes.push("sweat shirt");
+clothes.push("T-shirt");
+clothes.push("Pants");
+clothes.push("Tie");
+clothes.push("Jacket");
+console.log(clothes.pop());
+console.log(clothes);
+console.log(clothes.push("coat"));
+console.log(clothes);
+//----------------------------------------------------------------
+//Javascript methods
+var i;
+var car={};
+car.mileage=98765;
+car.color="red";
+console.log(car);
+car.turnTheKey = function(i)
+{
+    if(i==0){
+        console.log("The engine is running");
+    }
+    else{
+        console.log("The engine is not running");
+    }
+    
+}
+car.lightsOn=function(i)
+{
+    if(i==1)
+    {
+        console.log("The lights are on.");
+    }
+    else{
+        console.log("The lights are off.");
+    }
+}
+console.log(car);
+car.turnTheKey(0);
+car.lightsOn(0);
